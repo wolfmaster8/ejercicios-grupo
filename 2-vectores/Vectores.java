36eac555601b38ec5848977aa18254cb84dee6f2
sbarona20545 <jflobom@gmail.com>
2018-08-20 15:00:44 -0300

    Functions finished

--- 2-vectores/Vectores.java
+++ 2-vectores/Vectores.java
@@ -5,6 +5,7 @@
  * @version 0.1a
  */
 import java.util.Scanner;
+import java.util.Arrays;
 public class Vectores
 {
     public static void main(String[] args){
@@ -14,6 +15,7 @@ public class Vectores
         
         
         System.out.println("Matrices No Comunes");
+        
         System.out.println("Ingresa la cantidad de números de la matriz 1");
         cantidad = entrada.nextInt();
         int[] n1 = new int[cantidad];
@@ -41,15 +43,63 @@ public class Vectores
     
     public static void pequena(int[] n1, int[] n2){
         //matriz mas pequena
-        
         int len1, len2;
         len1 = n1.length;
         len2 = n2.length;
-        
+        //organiza matrices
+        Arrays.sort(n1);
+        Arrays.sort(n2);
         if(len1>=len2){
-            //evalua la matriz 2 en la 1
+            //evalua la matriz 2 en la 1. Es decir la mas pequena en la mas grande
+            recorredor(n1, n2, len1, len2);
         }else{
             //evalua la matriz 1 en la 2
         }
+    }
+    
+    public static boolean buscar( int[] matriz, int numero){
+        //System.out.println("Buscando> "+numero);
+        int index = Arrays.binarySearch(matriz, numero);
+        return index >= 0;
+        
+    }
+    
+    public static void recorredor(int[] grande, int[] pequena, int lenG, int lenP){
+        int[] solos = new int[lenG];
+        int contador =0;
+        boolean repetido;
+        //evalua la pequena y la grande
+        
+        
+        for(int c : grande){
+            
+            System.out.println(grande[c-1]);
+            repetido = buscar(pequena, grande[c-1]);
+            
+            //System.out.println("Buscando en Pequeña-->"+grande[c]);
+            if(repetido == false){
+                System.out.println("Grande->"+grande[c-1]+"<->NUEVO");
+                solos[contador] = grande[c-1];
+                contador ++;
+            }
+        };
+        System.out.println("--------sss-------");
+        for(int i : pequena){
+            
+            System.out.println(i);
+            repetido = buscar(grande, i);
+            
+            //System.out.println("Buscando en Pequeña-->"+grande[c]);
+            if(repetido == false){
+                System.out.println("Pequeña->"+i+"<->NUEVO");
+                solos[contador] = i;
+                contador ++;
+            }
+        };
+        System.out.println("---------------");
+        for(int c: solos){
+            System.out.println(c);
+        }
+        //System.out.println();
     }
 }
