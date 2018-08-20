5f0b0eecd7124d03a98634e9c64ca196bc47c816
wolfmaster8 <jflobom@gmail.com>
2018-08-20 10:27:38 -0300

    added class

---
+++ 1-Desviacion Estandar/Desviacion.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Desviacion here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+public class Desviacion
+{
+    public static void main(String[] args){
+      
+        
+    }
+}
