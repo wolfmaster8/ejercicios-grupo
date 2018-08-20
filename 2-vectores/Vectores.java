bac316ca3a2f458d20963775dfe6760589be83f4
sbarona20545 <jflobom@gmail.com>
2018-08-20 15:17:40 -0300

    Codigo organizado. Ejercicio terminado.

--- 2-vectores/Vectores.java
+++ 2-vectores/Vectores.java
@@ -38,11 +38,11 @@ public class Vectores
             cantidad--;
             contador++;
         }
-        pequena(n1, n2);
+        evalua(n1, n2);
     }
     
-    public static void pequena(int[] n1, int[] n2){
-        //matriz mas pequena
+    public static void evalua(int[] n1, int[] n2){
+        //Organiza las matrices. Evalua las matrices.
         int len1, len2;
         len1 = n1.length;
         len2 = n2.length;
@@ -51,49 +51,46 @@ public class Vectores
         Arrays.sort(n2);
         if(len1>=len2){
             //evalua la matriz 2 en la 1. Es decir la mas pequena en la mas grande
-            recorredor(n1, n2, len1, len2);
+            recorredor(n1, n2, len1);
         }else{
             //evalua la matriz 1 en la 2
-            recorredor(n2, n1, len2, len1);
+            recorredor(n2, n1, len2);
         }
     }
     
     public static boolean buscar( int[] matriz, int numero){
-        //System.out.println("Buscando> "+numero);
+        //Busca el valor en la matriz solicitada
         int index = Arrays.binarySearch(matriz, numero);
-        return index >= 0;
         
+        return index >= 0;
     }
     
-    public static void recorredor(int[] grande, int[] pequena, int lenG, int lenP){
+    public static void recorredor(int[] grande, int[] pequena, int lenG){
+        //Recorre ambas matrices y las compara. Crea la matriz C y la muestra
         int[] solos = new int[lenG];
         int contador =0;
         boolean repetido;
         //evalua la pequena y la grande
-        
-        
+
         for(int c : grande){
-            //System.out.println(c);
+            
             repetido = buscar(pequena, c);
             if(repetido == false){
-                //System.out.println("Grande->"+c+"<->NUEVO");
                 solos[contador] = c;
                 contador ++;
             }
-        };
+            
+        }
         
         for(int i : pequena){
             
-            //System.out.println(i);
             repetido = buscar(grande, i);
-            
-            //System.out.println("Buscando en Pequeña-->"+grande[c]);
             if(repetido == false){
-                //System.out.println("Pequeña->"+i+"<->NUEVO");
                 solos[contador] = i;
                 contador ++;
             }
-        };
+            
+        }
         System.out.println("---------------\nLos valores no repetidos son:\n");
         Arrays.sort(solos);
         for(int c: solos){
@@ -103,6 +100,5 @@ public class Vectores
             
         }
         System.out.println("---------------");
-        //System.out.println();
     }
 }
